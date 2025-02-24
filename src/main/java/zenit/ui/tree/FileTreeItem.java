331e3cc085dec00e820249b9b4c10f1dbddcf475
Yrja Mai Hoang <Yrjamai@outlook.com>
2025-02-24 12:57:14 +0100

    Refactoring of Tree folder
    Simplified and cleaned up the classes FileTree, FileTreeItem, TreeClickListener, and TreeContextMenu

--- src/main/java/zenit/ui/tree/FileTreeItem.java
+++ src/main/java/zenit/ui/tree/FileTreeItem.java
@@ -1,14 +1,22 @@
 package main.java.zenit.ui.tree;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 import javafx.scene.control.TreeItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+/**
+ * A custom TreeItem that holds a File object and an icon for the tree item.
+ * The type of the tree item is stored as an integer and is used to determine the icon.
+ * @param <T> The type of the value of the tree item
+ */
 public class FileTreeItem<T> extends TreeItem<T> {
 	private File file;
+	private ImageView icon;
 	private int type;
-	public static final int WORKSPACE = 100;
+	public static final int WORKSPACE = 100; // Arbitrary numbers to represent the type of the tree item
 	public static final int PROJECT = 101;
 	public static final int PACKAGE = 102;
 	public static final int CLASS = 103;
@@ -16,7 +24,30 @@ public class FileTreeItem<T> extends TreeItem<T> {
 	public static final int FOLDER = 105;
 	public static final int FILE = 106;
 	public static final int INCOMPATIBLE = 107;
-    private ImageView icon;
+
+	// Icons for the tree items stored in a map
+	private static final Map<Integer, String> ICON_URLS = new HashMap<>();
+	static {
+		ICON_URLS.put(PROJECT, "/zenit/ui/tree/project.png");
+		ICON_URLS.put(PACKAGE, "/zenit/ui/tree/package.png");
+		ICON_URLS.put(CLASS, "/zenit/ui/tree/class.png");
+		ICON_URLS.put(SRC, "/zenit/ui/tree/src.png");
+		ICON_URLS.put(FOLDER, "/zenit/ui/tree/folder.png");
+		ICON_URLS.put(FILE, "/zenit/ui/tree/file.png");
+		ICON_URLS.put(INCOMPATIBLE, "/zenit/ui/tree/incompatible.png");
+	}
+
+	// Strings for the types stored in a map
+	private static final Map<Integer, String> TYPE_STRINGS = new HashMap<>();
+	static {
+		TYPE_STRINGS.put(PROJECT, "project");
+		TYPE_STRINGS.put(PACKAGE, "package");
+		TYPE_STRINGS.put(CLASS, "class");
+		TYPE_STRINGS.put(SRC, "src-folder");
+		TYPE_STRINGS.put(FOLDER, "folder");
+		TYPE_STRINGS.put(FILE, "file");
+		TYPE_STRINGS.put(INCOMPATIBLE, "incompatible");
+	}
 	
 	public FileTreeItem(File file, T name, int type) {
 		super(name);
@@ -25,19 +56,13 @@ public class FileTreeItem<T> extends TreeItem<T> {
 		
 		setIcon();
 	}
-	
-	public void setIcon() {
-		String url = null;
-		switch(type) {
-		case PROJECT: url = "/zenit/ui/tree/project.png"; break;
-		case PACKAGE: url = "/zenit/ui/tree/package.png"; break;
-		case CLASS: url = "/zenit/ui/tree/class.png"; break;
-		case SRC: url = "/zenit/ui/tree/src.png"; break;
-		case FOLDER: url = "/zenit/ui/tree/folder.png"; break;
-		case FILE: url = "/zenit/ui/tree/file.png"; break;
-		case INCOMPATIBLE: url = "/zenit/ui/tree/incompatible.png"; break;
-		}
-		
+
+	/**
+	 * Sets the icon of the tree item based on the type of the item.
+	 * The icon is set to null if the type is not found in the map.
+	 */
+	private void setIcon() {
+		String url = ICON_URLS.get(type);
 		if (url != null) {
 			icon = new ImageView(new Image(getClass().getResource(url).toExternalForm()));
 			icon.setFitHeight(16);
@@ -52,19 +77,8 @@ public class FileTreeItem<T> extends TreeItem<T> {
 	public File getFile() { return file; }
 	
 	public int getType() { return type; }
-	
+
 	public String getStringType() {
-		String stringType;
-		switch (type) {
-		case PROJECT: stringType = "project"; break;
-		case PACKAGE: stringType = "package"; break;
-		case CLASS: stringType = "class"; break;
-		case SRC: stringType = "src-folder"; break;
-		case FOLDER: stringType = "folder"; break;
-		case FILE: stringType = "file"; break;
-		case INCOMPATIBLE: stringType = "incompatible"; break;
-		default: stringType = null;
-		}
-		return stringType;
+		return TYPE_STRINGS.get(type);
 	}
 }
--- src/main/java/zenit/ui/tree/TreeContextMenu.java
+++ src/main/java/zenit/ui/tree/TreeContextMenu.java
@@ -1,6 +1,10 @@
 package main.java.zenit.ui.tree;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
@@ -14,16 +18,16 @@ import main.java.zenit.filesystem.helpers.CodeSnippets;
 import main.java.zenit.ui.MainController;
 
 public class TreeContextMenu extends ContextMenu implements EventHandler<ActionEvent>{
-	private MainController controller;
-	private TreeView<String> treeView;
-	private Menu createItem = new Menu("New...");
-	private MenuItem createClass = new MenuItem("New class");
-	private MenuItem createInterface = new MenuItem("New interface");
-	private MenuItem createPackage = new MenuItem("New package");
-	private MenuItem renameItem = new MenuItem("Rename");
-	private MenuItem deleteItem = new MenuItem("Delete");
-	private MenuItem importJar = new MenuItem("Import jar");
-	private MenuItem properties = new MenuItem("Properties");
+	private final MainController controller;
+	private final TreeView<String> treeView;
+	private final Menu createItem = new Menu("New...");
+	private final MenuItem createClass = new MenuItem("New class");
+	private final MenuItem createInterface = new MenuItem("New interface");
+	private final MenuItem createPackage = new MenuItem("New package");
+	private final MenuItem renameItem = new MenuItem("Rename");
+	private final MenuItem deleteItem = new MenuItem("Delete");
+	private final MenuItem importJar = new MenuItem("Import jar");
+	private final MenuItem properties = new MenuItem("Properties");
 	
 	public TreeContextMenu(MainController controller, TreeView<String> treeView) {
 		super();
@@ -31,18 +35,25 @@ public class TreeContextMenu extends ContextMenu implements EventHandler<ActionE
 		this.treeView = treeView;
 		initContextMenu();
 	}
-	
+
+	/**
+	 * Sets the context of the context menu based on the selected node.
+	 * Updates the text of the rename and delete menu items to include the name of the selected node.
+	 * Adds or removes the "New package" menu item based on whether the selected node is the "src" folder.
+	 * Adds or removes the "Import jar" and "Properties" menu items based on whether the selected node is a project.
+	 *
+	 * @param selectedNode The name of the selected node
+	 */
 	private void setContext(String selectedNode) {
-		String renameItemTitle = String.format("Rename \"%s\"", selectedNode);
-		String deleteItemTitle = String.format("Delete \"%s\"", selectedNode);
-		renameItem.setText(renameItemTitle);
-		deleteItem.setText(deleteItemTitle);
-				
+		renameItem.setText(String.format("Rename \"%s\"", selectedNode));
+		deleteItem.setText(String.format("Delete \"%s\"", selectedNode));
+
 		if (selectedNode.equals("src") && !createItem.getItems().contains(createPackage)) {
 			createItem.getItems().add(createPackage);
 		} else {
 			createItem.getItems().remove(createPackage);
 		}
+
 		FileTreeItem<String> selectedItem = (FileTreeItem<String>) treeView.getSelectionModel().getSelectedItem();
 		if (selectedItem.getType() == FileTreeItem.PROJECT) {
 			getItems().add(importJar);
@@ -52,69 +63,156 @@ public class TreeContextMenu extends ContextMenu implements EventHandler<ActionE
 			getItems().remove(properties);
 		}
 	}
-	
+
+	/**
+	 * Shows the context menu at the specified coordinates.
+	 * @param node The node to show the context menu for
+	 * @param x The x-coordinate of the context menu
+	 * @param y The y-coordinate of the context menu
+	 */
 	@Override
 	public void show(Node node, double x, double y) {
 		TreeItem<String> selectedItem = treeView.getSelectionModel().getSelectedItem();
-		
 		if (selectedItem != null) { setContext(selectedItem.getValue()); }
-		
 		super.show(node, x, y);
 	}
-	
+
+	/**
+	 * Initializes the context menu by adding the menu items and setting their action handlers.
+	 */
 	private void initContextMenu() {
-		createItem.getItems().add(createClass);
-		createItem.getItems().add(createInterface);
+		createItem.getItems().addAll(createClass, createInterface);
 		getItems().addAll(createItem, renameItem, deleteItem);
-		createClass.setOnAction(this);
-		createInterface.setOnAction(this);
-		renameItem.setOnAction(this);
-		deleteItem.setOnAction(this);
-		createPackage.setOnAction(this);
-		importJar.setOnAction(this);
-		properties.setOnAction(this);
+		setMenuItemActionHandlers(createClass, createInterface, renameItem, deleteItem, createPackage, importJar, properties);
 	}
-	
+
+	/**
+	 * Sets the action handlers for the specified menu items.
+	 * @param menuItems The menu items to set the action handlers for
+	 */
+	private void setMenuItemActionHandlers(MenuItem... menuItems) {
+		for (MenuItem menuItem : menuItems) {
+			menuItem.setOnAction(this);
+		}
+	}
+
+	/**
+	 * Creates a new file of the specified type and adds it to the tree by calling its helper method.
+	 * @param typeCode The type code of the file to create
+	 */
 	private void newFile(int typeCode) {
-		FileTreeItem<String> parent = (FileTreeItem<String>) 
-				treeView.getSelectionModel().getSelectedItem();
+		createAndAddFileToTree(typeCode, FileTreeItem.CLASS);
+	}
+
+	/**
+	 * Creates a new file of the specified type and adds it to the tree.
+	 * @param typeCode The type code of the file to create
+	 * @param fileType The type of the file to add to the tree
+	 */
+	private void createAndAddFileToTree(int typeCode, int fileType) {
+		FileTreeItem<String> parent = (FileTreeItem<String>) treeView.getSelectionModel().getSelectedItem();
 		File newFile = controller.createFile(parent.getFile(), typeCode);
 		if (newFile != null) {
-			FileTreeItem<String> newItem = new FileTreeItem<String>(newFile, newFile.getName(), FileTreeItem.CLASS);
+			FileTreeItem<String> newItem = new FileTreeItem<>(newFile, newFile.getName(), fileType);
 			parent.getChildren().add(newItem);
 		}
 	}
 
+	/**
+	 * Handles action events triggered by the context menu.
+	 * Executes the corresponding action based on the menu item text.
+	 *
+	 * @param actionEvent The action event that triggered the handler
+	 */
 	@Override
 	public void handle(ActionEvent actionEvent) {
 		FileTreeItem<String> selectedItem = (FileTreeItem<String>) treeView.getSelectionModel().getSelectedItem();
 		File selectedFile = selectedItem.getFile();
-		
-		if (actionEvent.getSource().equals(createClass)) {
-			newFile(CodeSnippets.CLASS);
-		} else if (actionEvent.getSource().equals(createInterface)) {
-			newFile(CodeSnippets.INTERFACE);
-		} else if (actionEvent.getSource().equals(renameItem)) {
-			File newFile = controller.renameFile(selectedFile);
-			if (newFile != null) {
-				selectedItem.setFile(newFile);
-				selectedItem.setValue(newFile.getName());
-				FileTree.changeFileForNodes(selectedItem, selectedItem.getFile());
-			}
-		} else if (actionEvent.getSource().equals(deleteItem)) {
-			controller.deleteFile(selectedFile);
-			selectedItem.getParent().getChildren().remove(selectedItem);
-		} else if (actionEvent.getSource().equals(createPackage)) {
-			File packageFile = controller.newPackage(selectedFile);
-			if (packageFile != null) {
-				FileTreeItem<String> packageNode = new FileTreeItem<String>(packageFile, packageFile.getName(), FileTreeItem.PACKAGE);
-				selectedItem.getChildren().add(packageNode);
-			}
-		} else if (actionEvent.getSource().equals(importJar)) {
-			ProjectFile projectFile = new ProjectFile(selectedFile.getPath());
-			controller.chooseAndImportLibraries(projectFile);
-		} else if (actionEvent.getSource().equals(properties) && selectedItem.getType() == FileTreeItem.PROJECT) {
-			ProjectFile projectFile = new ProjectFile(selectedFile.getPath());
+
+		Map<String, Consumer<File>> actions = createActionsMap(selectedItem);
+
+		Object source = actionEvent.getSource();
+		if (source instanceof MenuItem) {
+			MenuItem menuItem = (MenuItem) source;
+			actions.getOrDefault(menuItem.getText(), f -> {}).accept(selectedFile);
+		}
+	}
+
+	/**
+	 * Creates a map of actions associated with the menu item texts.
+	 * @param selectedItem The selected item in the tree view
+	 * @return The map of actions
+	 */
+	private Map<String, Consumer<File>> createActionsMap(FileTreeItem<String> selectedItem) {
+		Map<String, Consumer<File>> actions = new HashMap<>();
+		actions.put("New class", file -> newFile(CodeSnippets.CLASS));
+		actions.put("New interface", file -> newFile(CodeSnippets.INTERFACE));
+		actions.put("Rename", file -> renameFile(selectedItem, file));
+		actions.put("Delete", file -> deleteFile(selectedItem, file));
+		actions.put("New package", file -> newPackage(selectedItem, file));
+		actions.put("Import jar", this::importJar);
+		actions.put("Properties", file -> showProperties(selectedItem, file));
+		return actions;
+	}
+
+	/**
+	 * Renames the selected file and updates the tree view.
+	 * @param selectedItem The selected item in the tree view
+	 * @param file The file to rename
+	 */
+	private void renameFile(FileTreeItem<String> selectedItem, File file) {
+		File newFile = controller.renameFile(file);
+		if (newFile != null) {
+			selectedItem.setFile(newFile);
+			selectedItem.setValue(newFile.getName());
+			FileTree.changeFileForNodes(selectedItem, selectedItem.getFile());
+		}
+	}
+
+	/**
+	 * Deletes the selected file and removes the tree item.
+	 *
+	 * @param selectedItem The selected item in the tree view
+	 * @param file The file to delete
+	 */
+	private void deleteFile(FileTreeItem<String> selectedItem, File file) {
+		controller.deleteFile(file);
+		selectedItem.getParent().getChildren().remove(selectedItem);
+	}
+
+	/**
+	 * Creates a new package and adds it to the tree.
+	 *
+	 * @param selectedItem The selected item in the tree view
+	 * @param file The file representing the package
+	 */
+	private void newPackage(FileTreeItem<String> selectedItem, File file) {
+		File packageFile = controller.newPackage(file);
+		if (packageFile != null) {
+			FileTreeItem<String> packageNode = new FileTreeItem<>(packageFile, packageFile.getName(), FileTreeItem.PACKAGE);
+			selectedItem.getChildren().add(packageNode);
+		}
+	}
+
+	/**
+	 * Imports a JAR file into the project.
+	 *
+	 * @param file The JAR file to import
+	 */
+	private void importJar(File file) {
+		ProjectFile projectFile = new ProjectFile(file.getPath());
+		controller.chooseAndImportLibraries(projectFile);
+	}
+
+	/**
+	 * Shows the properties of the selected project.
+	 *
+	 * @param selectedItem The selected item in the tree view
+	 * @param file The file representing the project
+	 */
+	private void showProperties(FileTreeItem<String> selectedItem, File file) {
+		if (selectedItem.getType() == FileTreeItem.PROJECT) {
+			ProjectFile projectFile = new ProjectFile(file.getPath());
 			controller.showProjectProperties(projectFile);
 		}
 	}

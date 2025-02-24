0891ec8fd2e0ec4284f254860b3dfe564f8abf78
jontem1 <jontem94@hotmail.com>
2025-02-24 12:31:18 +0100

    Updated unit-tests
    rewrote test to work on both mac and windows os.

--- src/test/java/main/java/zenit/javacodecompiler/ProcessBufferTest.java
+++ src/test/java/main/java/zenit/javacodecompiler/ProcessBufferTest.java
@@ -21,25 +21,49 @@ class ProcessBufferTest {
 
     @Test
     void testPut() throws IOException {
-        Process mockprocess = new ProcessBuilder("cmd", "/c", "echo", "test").start(); //this is for windows and our tests runs on "windows-latest"
+        String os = System.getProperty("os.name").toLowerCase();
+        Process mockprocess;
+        if (os.contains("mac os")){
+             mockprocess = new ProcessBuilder("echo", "test").start(); //this is for macOS and our tests runs on "windows-latest"
+        }
+        else {
+             mockprocess = new ProcessBuilder("cmd", "/c", "echo", "test").start(); //this is for windows and our tests runs on "windows-latest"
+        }
         processBuffer.put(mockprocess);
         assertFalse(processBuffer.isEmpty());
     }
 
+
     @Test
     void testGet() throws IOException{
-        Process mockprocess = new ProcessBuilder("cmd", "/c", "echo", "test").start(); //this is for windows and our tests runs on "windows-latest"
+        String os = System.getProperty("os.name").toLowerCase();
+        Process mockprocess;
+        if (os.contains("mac os")){
+             mockprocess = new ProcessBuilder("echo", "test").start(); //this is for macOS and our tests runs on "windows-latest"
+        }
+        else {
+             mockprocess = new ProcessBuilder("cmd", "/c", "echo", "test").start(); //this is for windows and our tests runs on "windows-latest"
+        }
         processBuffer.put(mockprocess);
         assertEquals(mockprocess, processBuffer.get(), "Should match");
     }
 
+
     @Test
     void testEmptyAfterGet() throws IOException {
-        Process mockprocess = new ProcessBuilder("cmd", "/c", "echo", "test").start(); //this is for windows and our tests runs on "windows-latest"
+        String os = System.getProperty("os.name").toLowerCase();
+        Process mockprocess;
+        if (os.contains("mac os")){
+             mockprocess = new ProcessBuilder("echo", "test").start(); //this is for macOS and our tests runs on "windows-latest"
+        }
+        else {
+             mockprocess = new ProcessBuilder("cmd", "/c", "echo", "test").start(); //this is for windows and our tests runs on "windows-latest"
+        }
         processBuffer.put(mockprocess);
         processBuffer.get();
         assertTrue(processBuffer.isEmpty());
     }
+
 
     //Might have to add more tests, Not sure everything is covered.
 }

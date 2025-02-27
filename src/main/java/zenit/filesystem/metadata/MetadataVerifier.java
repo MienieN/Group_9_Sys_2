c287d3d415be3f45c75c4b55be32526570ec699d
MienieN <153607888+MienieN@users.noreply.github.com>
2025-02-27 09:59:11 +0100

    refactor metadataVerifier class
    refactor: updated the metadataFileMissing  and metadataOutdated methods.
    
    docs: added Javadoc comments

--- src/main/java/zenit/filesystem/metadata/MetadataVerifier.java
+++ src/main/java/zenit/filesystem/metadata/MetadataVerifier.java
@@ -11,12 +11,13 @@ import main.java.zenit.filesystem.MetadataFileHandler;
  * version matches the latest expected version.
  */
 public class MetadataVerifier {
-	
+	// variables to store the verification status
 	public static final int VERIFIED = 0;
 	public static final int METADATA_FILE_MISSING = 1;
 	public static final int METADATA_OUTDATED = 2;
 	
-
+	// -----------------------------------------------------------------------
+	// methods:
 	public static int verify(Metadata metadata) {
 		
 		if (metadataFileMissing(metadata)) {
@@ -29,25 +30,32 @@ public class MetadataVerifier {
 		return VERIFIED;	
 	}
 	
+	/**
+	 * Checks if the metadata file associated with the given Metadata object
+	 * is missing, does not exist, or is a directory.
+	 *
+	 * @param metadata The Metadata object containing the reference to the metadata file.
+	 * @return true if the metadata file is missing, does not exist, or is a directory; false otherwise.
+	 */
 	private static boolean metadataFileMissing(Metadata metadata) {
 		File metadataFile = null;
+		
 		if (metadata != null) {
 			metadataFile = metadata.getFile();
 		}
-		if (metadataFile == null || !metadataFile.exists() || metadataFile.isDirectory()) {
-			return true;
-		}
-		
-		return false;	
+        return metadataFile == null || !metadataFile.exists() || metadataFile.isDirectory();
 	}
 
+	/**
+	 * Determines if the version of the specified Metadata object is outdated.
+	 * A metadata object is considered outdated if its version is either null
+	 * or does not match the latest expected version.
+	 *
+	 * @param metadata The Metadata object to check for version compatibility.
+	 * @return true if the metadata version is null or does not match the latest version; false otherwise.
+	 */
 	private static boolean metadataOutdated(Metadata metadata) {
 		String version = metadata.getVersion();
-		
-		if (version == null || !version.equals(MetadataFileHandler.LATEST_VERSION)) {
-			return true;
-		} else {
-			return false;
-		}
+        return version == null || ! version.equals(MetadataFileHandler.LATEST_VERSION);
 	}
 }

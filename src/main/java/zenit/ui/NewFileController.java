ee0f26ba16d4d6da6ad71bbcddbcc52f24aa4a10
mohakado98 <mohamad.kadoura.98@gmail.com>
2025-03-17 16:13:22 +0100

    Refactor NewFileController and fix the issue with creating a new file, which wasn't working.

--- src/main/java/zenit/ui/NewFileController.java
+++ src/main/java/zenit/ui/NewFileController.java
@@ -1,8 +1,5 @@
 package main.java.zenit.ui;
 
-import java.io.File;
-import java.io.IOException;
-
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -18,148 +15,189 @@ import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Controller class responsible for the "New File" creation window.
+ * This class allows users to create a new file with a specified name and file type
+ * in a specified workspace. It provides functionality to select a directory,
+ * handle dark mode, and manage the creation of new files.
+ */
 public class NewFileController extends AnchorPane {
-	
-	private Stage stage;
-	private File workspace, newFile;
-	private boolean darkMode;
-	private double xOffset = 0, yOffset = 0;
-	@FXML private ImageView logo;
-	@FXML private AnchorPane header;
-	@FXML private ListView<String> filePath;
-	@FXML private ComboBox<String> fileEnding;
-	@FXML private TextField textFieldName;
-	
-	public NewFileController(File workspace, boolean darkMode) {
-		this.workspace = workspace;
-		this.darkMode = darkMode;
-	}
-	
-	public void start() {
-		try {
-			//setup scene
-			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(getClass().getResource("/zenit/ui/NewFile.fxml"));
-			loader.setController(this);
-			AnchorPane root = (AnchorPane) loader.load();
-			Scene scene = new Scene(root);
 
-			//set up stage
-			stage = new Stage();
-			stage.setResizable(false);
-			stage.initStyle(StageStyle.UNDECORATED);
-			stage.setScene(scene);
-			
-			initialize();
-			ifDarkModeChanged(darkMode);
-			stage.showAndWait();
-				
-		} catch (IOException e) {
-			System.out.println("Error in NewFileController start() = " + e);
-		}
-	}
+    private Stage stage;
+    private boolean darkmode;
 
-	// YRJA: Broke this down into smaller methods for readability
-	private void initialize() {
-		initializeLogoFilePathAndEnding();
-		initializeHeader();
-	}
+    @FXML
+    private ImageView logo;
+    @FXML
+    private AnchorPane header;
+    @FXML
+    private ListView<String> filepath;
+    @FXML
+    private ComboBox<String> fileEnding;
+    @FXML
+    private TextField tfName;
 
-	private void initializeLogoFilePathAndEnding() {
-		logo.setImage(new Image(getClass().getResource("/zenit/setup/zenit.png").toExternalForm()));
-		logo.setFitWidth(45);
+    private double xOffset = 0;
+    private double yOffset = 0;
 
-		filePath.getItems().clear();
-		filePath.getItems().add(workspace.getPath());
-		filePath.getSelectionModel().selectFirst();
+    private File workspace;
+    private File newFile;
 
-		fileEnding.getItems().add(".txt");
-		fileEnding.getItems().add(".java");
-		fileEnding.getSelectionModel().select(".java");
-	}
+    /**
+     * Constructor for NewFileController, initializing workspace and dark mode settings.
+     *
+     * @param workspace The workspace directory where the new file will be created.
+     * @param darkmode A boolean indicating whether dark mode is enabled or not.
+     */
+    public NewFileController(File workspace, boolean darkmode) {
+        this.workspace = workspace;
+        this.darkmode = darkmode;
+    }
 
-	private void initializeHeader() {
-		header.setOnMousePressed(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent event) {
-				xOffset = event.getSceneX();
-				yOffset = event.getSceneY();
-			}
-		});
+    /**
+     * Opens the "New File" window and displays the UI to create a new file.
+     * The window includes fields for entering a filename, selecting a file type,
+     * and specifying the file's location.
+     */
+    public void start() {
+        try {
+            // Setup scene and load the FXML layout for the NewFile window
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/zenit/ui/NewFile.fxml"));
+            loader.setController(this);
+            AnchorPane root = (AnchorPane) loader.load();
+            Scene scene = new Scene(root);
 
-		header.setOnMouseDragged(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent event) {
-				stage.setX(event.getScreenX() - xOffset);
-				stage.setY(event.getScreenY() - yOffset);
-			}
-		});
-	}
-	
-	@FXML
-	private void create() {
-		String filename = textFieldName.getText();
+            // Set up the stage (window) for displaying the scene
+            stage = new Stage();
+            stage.setResizable(false);
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.setScene(scene);
 
-		if (!filename.equals("")) {
-			filename += fileEnding.getSelectionModel().getSelectedItem();
-			String filePath = this.filePath.getSelectionModel().getSelectedItem() + File.separator + filename;
-			newFile = new File(filePath);
-			try {
-				if (!newFile.createNewFile()) {
-					showErrorDialog("File name already exist", "A file with the name " + filename
-							+ " already exist. Please input a different name.");
-					newFile = null;
-				}
-			} catch (IOException e) {
-				showErrorDialog("Couldn't create new file", "Couldn't create new file");
-				newFile = null;
-			}
-			stage.close();
-		} else {
-			showErrorDialog("No name selected", "No name has been given to the new file. " +
-					"Please input a new name to create file.");
-		}
-	}
+            initialize();
+            ifDarkModeChanged(darkmode);
+            stage.showAndWait();
 
-	private void showErrorDialog(String title, String content) {
-		DialogBoxes.errorDialog(title, "", content);
-	}
-	
-	@FXML
-	private void cancel() { stage.close(); }
-	
-	@FXML
-	private void browse() {
-		DirectoryChooser dc = new DirectoryChooser();
-		dc.setInitialDirectory(workspace);
-		dc.setTitle("Select directory to create new file in");
-		File chosen = dc.showDialog(stage);
-		
-		if (chosen != null) {
-			filePath.getItems().clear();
-			filePath.getItems().add(chosen.getPath());
-			filePath.getSelectionModel().selectFirst();
-		}
-	}
-	
-	public void ifDarkModeChanged(boolean isDarkMode) {
-		var stylesheets = stage.getScene().getStylesheets();
-		var darkMode = getClass().getResource("/zenit/ui/projectinfo/mainStyle.css").toExternalForm();
-		var lightMode = getClass().getResource("/zenit/ui/projectinfo/mainStyle-lm.css").toExternalForm();
-		
-		if (isDarkMode) {
-			if (stylesheets.contains(lightMode)) {
-				stylesheets.remove(lightMode);
-			}
-			
-			stylesheets.add(darkMode);
-		} else {
-			if (stylesheets.contains(darkMode)) {
-				stylesheets.remove(darkMode);
-			}
-			stylesheets.add(lightMode);
-		}	
-	}
-	
-	public File getNewFile() { return newFile; }
+        } catch (IOException e) {
+
+        }
+
+    }
+
+    /**
+     * Initializes the UI components with default values and behavior.
+     * Sets the workspace path, file types, and drag-and-drop behavior for the window header.
+     */
+    private void initialize() {
+        logo.setImage(new Image(getClass().getResource("/zenit/setup/zenit.png").toExternalForm()));
+        logo.setFitWidth(45);
+
+        filepath.getItems().clear();
+        filepath.getItems().add(workspace.getPath());
+        filepath.getSelectionModel().selectFirst();
+
+        fileEnding.getItems().add(".txt");
+        fileEnding.getItems().add(".java");
+        fileEnding.getSelectionModel().select(".java");
+
+        header.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+
+        //move around here
+        header.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stage.setX(event.getScreenX() - xOffset);
+                stage.setY(event.getScreenY() - yOffset);
+            }
+        });
+    }
+
+    /**
+     * Creates a new file with the name and file extension specified by the user.
+     * It checks if the file already exists, and if not, creates the file in the chosen directory.
+     * If any errors occur, an error dialog is displayed.
+     */
+    @FXML
+    private void create() {
+        String filename = tfName.getText();
+
+        if (!filename.equals("")) {
+            filename += fileEnding.getSelectionModel().getSelectedItem();
+
+            String filePath = this.filepath.getSelectionModel().getSelectedItem() + File.separator + filename;
+            newFile = new File(filePath);
+            try {
+                if (!newFile.createNewFile()) {
+                    DialogBoxes.errorDialog("File name already exist", "", "A file with the name " + filename + " already exist. Please input a different name.");
+                    newFile = null;
+                }
+            } catch (IOException e) {
+                DialogBoxes.errorDialog("Couldn't create new file", "", "Couldn't create new file");
+                newFile = null;
+            }
+
+            stage.close();
+        } else {
+            DialogBoxes.errorDialog("No name selected", "", "No name has been given to the new file" + ". Please input a new name to create file.");
+        }
+
+    }
+
+    /**
+     * Closes the "New File" window without creating a file.
+     */
+    @FXML
+    private void cancel() {
+        stage.close();
+    }
+
+    /**
+     * Opens a directory chooser dialog to select a directory in which to create the new file.
+     * Updates the filepath field with the selected directory.
+     */
+    @FXML
+    private void browse() {
+        DirectoryChooser dc = new DirectoryChooser();
+        dc.setInitialDirectory(workspace);
+        dc.setTitle("Select directory to create new file in");
+        File chosen = dc.showDialog(stage);
+
+        if (chosen != null) {
+            filepath.getItems().clear();
+            filepath.getItems().add(chosen.getPath());
+            filepath.getSelectionModel().selectFirst();
+        }
+    }
+
+    /**
+     * Updates the style of the window based on whether dark mode is enabled.
+     *
+     * @param isDarkMode A boolean indicating whether dark mode is enabled or not.
+     */
+    public void ifDarkModeChanged(boolean isDarkMode) {
+        var stylesheets = stage.getScene().getStylesheets();
+        var darkMode = getClass().getResource("/zenit/ui/projectinfo/mainStyle.css").toExternalForm();
+        var lightMode = getClass().getResource("/zenit/ui/projectinfo/mainStyle-lm.css").toExternalForm();
+
+        stylesheets.removeIf(s -> s.equals(darkMode) || s.equals(lightMode)); // Remove both first
+        stylesheets.add(isDarkMode ? darkMode : lightMode); // Add the correct one
+    }
+
+    /**
+     * Retrieves the newly created file.
+     *
+     * @return The newly created file, or null if no file was created.
+     */
+    public File getNewFile() {
+        return newFile;
+    }
 }

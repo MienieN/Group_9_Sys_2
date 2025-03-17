365012e8e5e394ce6eabc8c420f336eb644a7513
mohakado98 <mohamad.kadoura.98@gmail.com>
2025-03-17 03:59:47 +0100

    Refactor the following classes: WorkspaceHandler, RunnableClass, Buffer (interface) and StreamRedirector.

--- src/main/java/zenit/filesystem/WorkspaceHandler.java
+++ src/main/java/zenit/filesystem/WorkspaceHandler.java
@@ -1,70 +1,86 @@
 package main.java.zenit.filesystem;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 
+/**
+ * Handles workspace file creation, reading, and setup for a new workspace.
+ */
 public class WorkspaceHandler {
-	
-	private static void createWorkspaceFile() {
-		File workspaceFile = new File("res/workspace/workspace.dat");
-		
-		if (!workspaceFile.exists()) {
-			workspaceFile.getParentFile().mkdirs();
 
-			try {
-				workspaceFile.createNewFile();
-			} catch (IOException ex) {
-				ex.printStackTrace();
-			}
-		}
-	}
+    /**
+     * Creates the workspace file if it does not exist.
+     */
+    private static void createWorkspaceFile() {
+        File workspaceFile = new File("res/workspace/workspace.dat");
 
-	public static boolean createWorkspace(File file) {
-		boolean success = false;
-				
-		createWorkspaceFile();
-		
-		try (ObjectOutputStream oos = new ObjectOutputStream(new BufferedOutputStream(
-				new FileOutputStream("res/workspace/workspace.dat")))) {
-			oos.writeObject(file);
-			oos.flush();
-			success = true;
-		} catch (IOException ex) {
-			System.err.println("WorkspaceHandler.createWorkspace: IOException: " + ex.getMessage());
-		}
-		return success;
-	}
+        if (!workspaceFile.exists()) {
+            workspaceFile.getParentFile().mkdirs();
 
-	public static File readWorkspace() throws IOException {
-		File workspace = null;
-		try (ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(
-				new FileInputStream("res/workspace/workspace.dat")))) {
-			workspace = (File) ois.readObject();
-		} catch (ClassNotFoundException ex) {
-			System.err.println("WorkspaceHandler.readWorkspace: ClassNotFoundException " + ex.getMessage());
-		}
-		return workspace;
-	}
+            try {
+                workspaceFile.createNewFile();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
 
-	// TODO Creates a new workspace when user starts the program for the first time instead of asking them to choose a workspace.
-	//  Uses the users desktop to save a new folder named zenit to store the new project in.
-	//  Maybe make this a select folder dialog instead?
-	public static File setUpNewWorkspace() {
-		String userHome = System.getProperty("user.home");
-		File desktop = new File(userHome, "Desktop");
-		File zenitDir = new File(desktop, "Zenit");
+    /**
+     * Creates a workspace by serializing the given file into the workspace file.
+     *
+     * @param file The file representing the workspace.
+     * @return True if the workspace file was successfully created, false otherwise.
+     */
+    public static boolean createWorkspace(File file) {
+        boolean success = false;
 
-		if (!zenitDir.exists()) {
-			zenitDir.mkdirs();
-		}
+        createWorkspaceFile();
 
-		return zenitDir;
-	}
+        try (ObjectOutputStream oos = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream("res/workspace/workspace.dat")))) {
+            oos.writeObject(file);
+            oos.flush();
+            success = true;
+        } catch (IOException ex) {
+            System.err.println("WorkspaceHandler.createWorkspace: IOException: " + ex.getMessage());
+        }
+        return success;
+    }
+
+    /**
+     * Reads the workspace file and deserializes it to return the workspace file.
+     *
+     * @return The deserialized workspace file, or null if an error occurred.
+     * @throws IOException If there is an error reading the workspace file.
+     */
+    public static File readWorkspace() throws IOException {
+        File workspace = null;
+        try (ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(new FileInputStream("res/workspace/workspace.dat")))) {
+            workspace = (File) ois.readObject();
+        } catch (ClassNotFoundException ex) {
+            System.err.println("WorkspaceHandler.readWorkspace: ClassNotFoundException " + ex.getMessage());
+        }
+        return workspace;
+    }
+
+    // TODO Creates a new workspace when user starts the program for the first time instead of asking them to choose a workspace.
+    //  Uses the users desktop to save a new folder named zenit to store the new project in.
+    //  Maybe make this a select folder dialog instead?
+
+    /**
+     * Sets up a new workspace by creating a "Zenit" directory on the user's desktop.
+     * If the directory already exists, it simply returns the directory.
+     *
+     * @return The Zenit directory representing the new workspace.
+     */
+    public static File setUpNewWorkspace() {
+        String userHome = System.getProperty("user.home");
+        File desktop = new File(userHome, "Desktop");
+        File zenitDir = new File(desktop, "Zenit");
+
+        if (!zenitDir.exists()) {
+            zenitDir.mkdirs();
+        }
+
+        return zenitDir;
+    }
 
 }
--- src/main/java/zenit/javacodecompiler/Buffer.java
+++ src/main/java/zenit/javacodecompiler/Buffer.java
@@ -1,11 +1,31 @@
 package main.java.zenit.javacodecompiler;
 
+/**
+ * A simple generic interface for a buffer that can store and retrieve elements.
+ *
+ * @param <C> The type of the elements in the buffer.
+ */
 public interface Buffer<C> {
-	
-	public void put(C element);
-	
-	public C get();
-	
-	public boolean isEmpty();
+
+    /**
+     * Adds an element to the buffer.
+     *
+     * @param element The element to add to the buffer.
+     */
+    public void put(C element);
+
+    /**
+     * Retrieves an element from the buffer.
+     *
+     * @return The element retrieved from the buffer.
+     */
+    public C get();
+
+    /**
+     * Checks if the buffer is empty.
+     *
+     * @return true if the buffer is empty, false otherwise.
+     */
+    public boolean isEmpty();
 
 }

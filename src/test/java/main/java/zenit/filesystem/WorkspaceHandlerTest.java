f1bde0c62cba96e0efdf0317dfdcd28f476ba227
MienieN <153607888+MienieN@users.noreply.github.com>
2025-03-21 16:53:27 +0100

    WorkspaceHandler Test
    test: added unit tests for WorkspaceHandler class
    Co-Authored-By: Yrja Mai Hoang <140621667+Yrjamai@users.noreply.github.com>

---
+++ src/test/java/main/java/zenit/filesystem/WorkspaceHandlerTest.java
@@ -0,0 +1,38 @@
+package main.java.zenit.filesystem;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class WorkspaceHandlerTest {
+    private WorkspaceHandler workspaceHandler;
+    
+    @BeforeEach
+    void setUp ( ) {
+        workspaceHandler = spy(new WorkspaceHandler());
+    }
+    
+    @Test
+    void createWorkspace ( ) {
+        boolean success = workspaceHandler.createWorkspace(null);
+        verify(workspaceHandler, times(1)).createWorkspace(null);
+        assertEquals(success, true);
+    }
+    
+    @Test
+    void readWorkspace ( ) throws IOException {
+        File file = workspaceHandler.readWorkspace();
+        assertNull(file);
+    }
+    
+    @Test
+    void setUpNewWorkspace ( ) {
+        File file = workspaceHandler.setUpNewWorkspace();
+        assertNotNull(file);
+    }
+}
